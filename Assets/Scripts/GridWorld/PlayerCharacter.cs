76d0926a849d3e6cb2ec91dc0c1edd4282e397f7
kvelury <kvelury@gmail.com>
2014-03-09 18:33:57 -0700

    Turn-based System added
    with the fix from ian.

--- Assets/Scripts/GridWorld/PlayerCharacter.cs
+++ Assets/Scripts/GridWorld/PlayerCharacter.cs
@@ -4,12 +4,14 @@ using System.Collections;
 public class PlayerCharacter : Entity {
 
 	public Move[] movelist;
+	public TurnManager tm;
 	private int currCombo;
-	private bool executeMode;
+	public bool executeMode;
 	private bool exit;
 	private int timer;
 	// Use this for initialization
 	protected override void Start () {
+		tm = GameObject.Find("lamePC").GetComponent<TurnManager>();
 		exit = false;
 		timer = 30;
 		executeMode = false;
@@ -26,76 +28,74 @@ public class PlayerCharacter : Entity {
 
 	// Update is called once per frame
 	void Update () {
-
-		if(!executeMode){
-			if (Input.GetKeyDown (KeyCode.LeftArrow)) {
-				if(currCombo < 10){
-					movelist [currCombo] = Move.Left;
-					currCombo++;
-					Debug.Log ("Left move queued.");
+				if (tm.turn == 1) {
+						if (!executeMode) {
+								if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+										if (currCombo < 10) {
+												movelist [currCombo] = Move.Left;
+												currCombo++;
+												Debug.Log ("Left move queued.");
+										} else
+												Debug.Log ("Queue full!");
+										//AttemptMove (Move.Left);
+								}
+								if (Input.GetKeyDown (KeyCode.RightArrow)) {
+										if (currCombo < 10) {
+												movelist [currCombo] = Move.Right;
+												currCombo++;
+												Debug.Log ("Right move queued.");
+										} else
+												Debug.Log ("Queue full!");
+										//AttemptMove (Move.Right);
+								}
+								if (Input.GetKeyDown (KeyCode.UpArrow)) {
+										if (currCombo < 10) {
+												movelist [currCombo] = Move.Up;
+												currCombo++;
+												Debug.Log ("Up move queued.");
+										} else
+												Debug.Log ("Queue full!");
+										//AttemptMove (Move.Up);
+								}
+								if (Input.GetKeyDown (KeyCode.DownArrow)) {
+										if (currCombo < 10) {
+												movelist [currCombo] = Move.Down;
+												currCombo++;
+												Debug.Log ("Down move queued.");
+										} else
+												Debug.Log ("Queue full!");
+										//AttemptMove (Move.Down);
+								}
+								if (Input.GetKeyDown (KeyCode.Space)) {
+										currCombo = 0;
+										executeMode = true;
+										tm.turn = 1;
+										Debug.Log ("Execute mode entered.");
+								}
+						} else {
+								if (currCombo > 9 || currCombo < 0) {
+										currCombo = 0;
+										exit = true;
+								}
+								if (exit || movelist [currCombo] == Move.None) {
+										for (int i = 0; i < 10; i++) {
+												movelist [i] = Move.None;
+										}
+										Debug.Log ("Execute mode exited.");
+										exit = false;
+										currCombo = 0;
+										timer = 30;
+										executeMode = false;
+										tm.turn = 2;
+										return;
+								}
+								timer++;
+								if (timer > 30) {
+										AttemptMove (movelist [currCombo]);
+										currCombo++;
+										timer = 0;
+								}
+						}
 				}
-				else
-					Debug.Log ("Queue full!");
-				//AttemptMove (Move.Left);
-			}
-			if (Input.GetKeyDown (KeyCode.RightArrow)){
-				if(currCombo < 10){
-				movelist [currCombo] = Move.Right;
-				currCombo++;
-				Debug.Log ("Right move queued.");
-				}
-				else
-					Debug.Log ("Queue full!");
-				//AttemptMove (Move.Right);
-			}
-			if (Input.GetKeyDown (KeyCode.UpArrow)) {
-				if(currCombo < 10){
-					movelist [currCombo] = Move.Up;
-					currCombo++;
-					Debug.Log ("Up move queued.");
-				}
-				else
-					Debug.Log ("Queue full!");
-				//AttemptMove (Move.Up);
-			}
-			if (Input.GetKeyDown (KeyCode.DownArrow)) {
-				if(currCombo < 10){
-					movelist [currCombo] = Move.Down;
-					currCombo++;
-					Debug.Log ("Down move queued.");
-				}
-				else
-					Debug.Log ("Queue full!");
-				//AttemptMove (Move.Down);
-			}
-			if(Input.GetKeyDown (KeyCode.Space)){
-				currCombo = 0;
-				executeMode = true;
-				Debug.Log ("Execute mode entered.");
-			}
 		}
-		else{
-			if(currCombo > 9 || currCombo < 0){
-				currCombo = 0;
-				exit = true;
-			}
-			if(exit || movelist[currCombo] == Move.None){
-				for(int i = 0; i < 10; i++){
-					movelist[i] = Move.None;
-				}
-				Debug.Log ("Execute mode exited.");
-				exit = false;
-				currCombo = 0;
-				timer = 30;
-				executeMode = false;
-				return;
-			}
-			timer++;
-			if(timer > 30){
-				AttemptMove(movelist[currCombo]);
-				currCombo++;
-				timer = 0;
-			}
-		}
-	}
 }
