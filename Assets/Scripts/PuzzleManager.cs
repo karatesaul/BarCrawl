2fdcee30126dd4db66d0374c0309cb9dd6a33e2b
capstoneCrusher <mtmcgowa@ucsc.edu>
2014-03-10 19:13:31 -0700

    PuzzleManagerFixes
    Changed the structure of Puzzle Manager from Pushing Into an Array to
    Pushing Into a List Structure of Move Types. Also general logic and code
    cleanup, getting the algorithm to work for all cases of the puzzle
    board.

--- Assets/Scripts/PuzzleManager.cs
+++ Assets/Scripts/PuzzleManager.cs
@@ -6,14 +6,16 @@ using System.Collections.Generic;
 /// The script to manage the puzzle, drawing the puzzle, and running the puzzle
 /// </summary>
 public class PuzzleManager : MonoBehaviour {
-	int scalingFactor;
 	
 	private Token[,] puzzleGrid;
 
 	/// <summary>
 	/// A queue of the matches found to be sent to the player movement.
 	/// </summary>
-	public List<Match> matches;
+	/// 
+	//public List<Match> matches;
+
+	public List<TokenType> setOfMoves;
 	
 	// Use this for initialization
 	void Start () {
@@ -24,106 +26,109 @@ public class PuzzleManager : MonoBehaviour {
 				puzzleGrid[i,j] = new Token(i, j, TokenType.Up);
 			}
 		}
-		matches = new List<Match> ();
+		//matches = new List<Match> ();
+
+		//List of moves to pass to the Game Board
+		setOfMoves = new List<TokenType> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		int slotNum = 0;
-		for (int i = 0; i < 6; i++){
-			if (puzzleGrid[i,2].tokenVal.Equals(puzzleGrid[i,1])){
-				if (puzzleGrid[i,1].tokenVal.Equals(puzzleGrid[i,0])){
-					matches[slotNum].firstNum = i;
-					matches[slotNum].secondNum = i+6;
-					matches[slotNum].thirdNum = i+12;
-					puzzleGrid[i,2].used = true;
-					puzzleGrid[i,1].used = true;
-					puzzleGrid[i,0].used = true;
-					slotNum++;
-			}
-				else if (puzzleGrid[i,2].tokenVal.Equals(puzzleGrid[i,3])){
-					matches[slotNum].firstNum = i+6;
-					matches[slotNum].secondNum = i+12;
-					matches[slotNum].thirdNum = i+18;
-					puzzleGrid[i,3].used = true;
-					puzzleGrid[i,2].used = true;
-					puzzleGrid[i,1].used = true;
-					slotNum++;
-				}
-		}
-			if (puzzleGrid[i,2].tokenVal.Equals(puzzleGrid[i,3])){
-				if (puzzleGrid[i,3].tokenVal.Equals(puzzleGrid[i,4])){
-					matches[slotNum].firstNum = i+12;
-					matches[slotNum].secondNum = i+18;
-					matches[slotNum].thirdNum = i+24;
-					puzzleGrid[i,4].used = true;
-					puzzleGrid[i,3].used = true;
-					puzzleGrid[i,2].used = true;
-					slotNum++;
-				}
-			}
-	}
-		for (int j = 0; j < 5; j++){
-			if (puzzleGrid[2,j].tokenVal.Equals(puzzleGrid[1,j])){
-				if (puzzleGrid[1,j].Equals(puzzleGrid[0,j])){
-					matches[slotNum].firstNum = 0+(6*j);
-					matches[slotNum].secondNum = 1+(6*j);
-					matches[slotNum].thirdNum = 2+(6*j);
-					puzzleGrid[2,j].used = true;
-					puzzleGrid[1,j].used = true;
-					puzzleGrid[0,j].used = true;
-					slotNum++;
-			}
-				else if (puzzleGrid[2,j].Equals(puzzleGrid[3,j])) {
-					matches[slotNum].firstNum = 1+(6*j);
-					matches[slotNum].secondNum = 2+(6*j);
-					matches[slotNum].thirdNum = 3+(6*j);
-					puzzleGrid[3,j].used = true;
-					puzzleGrid[2,j].used = true;
-					puzzleGrid[1,j].used = true;
-					slotNum++;
-			}
-		}
-	}
-		for (int j = 0; j < 5; j++){
-			if (puzzleGrid[3,j].tokenVal.Equals(puzzleGrid[4,j])){
-				if (puzzleGrid[4,j].Equals(puzzleGrid[5,j])){
-					matches[slotNum].firstNum = 3+(6*j);
-					matches[slotNum].secondNum = 4+(6*j);
-					matches[slotNum].thirdNum = 5+(6*j);
-					puzzleGrid[5,j].used = true;
-					puzzleGrid[4,j].used = true;
-					puzzleGrid[3,j].used = true;
-					slotNum++;
-				}
-				else if (puzzleGrid[3,j].Equals(puzzleGrid[2,j])) {
-					matches[slotNum].firstNum = 2+(6*j);
-					matches[slotNum].secondNum = 3+(6*j);
-					matches[slotNum].thirdNum = 4+(6*j);
-					puzzleGrid[4,j].used = true;
-					puzzleGrid[3,j].used = true;
-					puzzleGrid[2,j].used = true;
-					slotNum++;
-				}
-			}
-		}
 	}
 
-	public void makeMove (Match[] matches, Token[,] puzzleGrid){
+	public void makeMove (Token[,] puzzleGrid){
 		for (int i=0; i<20; i++){ //Maybe this works, maybe it doesn't, nobody knows!
 			//Pass along the matches queue, but don't organize it at this time.
 
 	}
 		//Move tiles down after matches.
 		for (int i=0; i<6; i++){
-			for (int j=0; j<4; j++){
+			for (int j=0; j<5; j++){
 				if (puzzleGrid[i,j].used == true){
-					puzzleGrid[i,j].tokenVal = puzzleGrid[i,j+1].tokenVal;
+					if (j<4){
+						puzzleGrid[i,j].tokenVal = puzzleGrid[i,j+1].tokenVal;
+					}
 					puzzleGrid[i,5].tokenVal = 0;
 				}
 			}
 		}
 		//Pass matches queue to GridMovement here.
+	}
+
+	public bool queueMove (){
+		int slotNum = 0;
+		bool foundMove = false;
+		for (int i = 0; i < 6; i++){
+			if (puzzleGrid[i,2].tokenVal.Equals(puzzleGrid[i,1])){
+				if (puzzleGrid[i,1].tokenVal.Equals(puzzleGrid[i,0])){
+					puzzleGrid[i,2].used = true;
+					puzzleGrid[i,1].used = true;
+					puzzleGrid[i,0].used = true;
+					foundMove = true;
+					setOfMoves.Add(puzzleGrid[i,2].tokenVal);
+					slotNum++;
+				}
+				if (puzzleGrid[i,2].tokenVal.Equals(puzzleGrid[i,3])){
+					puzzleGrid[i,3].used = true;
+					puzzleGrid[i,2].used = true;
+					puzzleGrid[i,1].used = true;
+					setOfMoves.Add(puzzleGrid[i,2].tokenVal);
+					foundMove = true;
+					slotNum++;
+				}
+			}
+			if (puzzleGrid[i,2].tokenVal.Equals(puzzleGrid[i,3])){
+				if (puzzleGrid[i,3].tokenVal.Equals(puzzleGrid[i,4])){
+					puzzleGrid[i,4].used = true;
+					puzzleGrid[i,3].used = true;
+					puzzleGrid[i,2].used = true;
+					setOfMoves.Add(puzzleGrid[i,2].tokenVal);
+					foundMove = true;
+					slotNum++;
+				}
+			}
+		}
+		for (int j = 0; j < 5; j++){
+			if (puzzleGrid[2,j].tokenVal.Equals(puzzleGrid[1,j])){
+				if (puzzleGrid[1,j].Equals(puzzleGrid[0,j])){
+					puzzleGrid[2,j].used = true;
+					puzzleGrid[1,j].used = true;
+					puzzleGrid[0,j].used = true;
+					setOfMoves.Add(puzzleGrid[2,j].tokenVal);
+					foundMove = true;
+					slotNum++;
+				}
+				if (puzzleGrid[2,j].Equals(puzzleGrid[3,j])) {
+					puzzleGrid[3,j].used = true;
+					puzzleGrid[2,j].used = true;
+					puzzleGrid[1,j].used = true;
+					setOfMoves.Add(puzzleGrid[2,j].tokenVal);
+					foundMove = true;
+					slotNum++;
+				}
+			}
+		}
+		for (int j = 0; j < 5; j++){
+			if (puzzleGrid[3,j].tokenVal.Equals(puzzleGrid[4,j])){
+				if (puzzleGrid[4,j].Equals(puzzleGrid[5,j])){
+					puzzleGrid[5,j].used = true;
+					puzzleGrid[4,j].used = true;
+					puzzleGrid[3,j].used = true;
+					setOfMoves.Add(puzzleGrid[3,j].tokenVal);
+					foundMove = true;
+					slotNum++;
+				}
+				if (puzzleGrid[3,j].Equals(puzzleGrid[2,j])) {
+					puzzleGrid[4,j].used = true;
+					puzzleGrid[3,j].used = true;
+					puzzleGrid[2,j].used = true;
+					setOfMoves.Add(puzzleGrid[3,j].tokenVal);
+					foundMove = true;
+					slotNum++;
+				}
+			}
+		}
+		return foundMove;
 	}
 }
 
@@ -136,10 +141,6 @@ public class Token{
 
 	public bool seen;
 	public bool used;
-
-	//remove these - use GameObject
-	public int xLocation;
-	public int yLocation;
 
 	public TokenType tokenVal;
 	public GameObject gameObject;
@@ -170,6 +171,7 @@ public class Token{
 	}
 }
 
+/*
 public class Match{
 
 	public int firstNum;
@@ -182,5 +184,12 @@ public class Match{
 		thirdNum = 0;
 	}
 }
+*/
 
 public enum TokenType : int { Empty, Up, Down, Left, Right, Attack };
+
+/* Example of format for Match Code!
+ * matches[slotNum].firstNum = 1+(6*j);
+ * matches[slotNum].secondNum = 2+(6*j);
+ * matches[slotNum].thirdNum = 3+(6*j);
+ */
