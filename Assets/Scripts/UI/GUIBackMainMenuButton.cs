f2d1fb13bb1aaec93ab214c6f8e665b972024a46
imacleis <imacleis@ucsc.edu>
2014-04-06 02:38:34 -0700

    Added a UI flow
    Scenes for all the ui. Back buttons for all scenes. WORKING LOCAL
    HIGHSCORES! :D... Which will need to take a global score variable which
    can be saved locally.

---
+++ Assets/Scripts/UI/GUIBackMainMenuButton.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUIBackMainMenuButton : MonoBehaviour {
+
+	//public AudioClip buttonPressClip;
+	
+	public Texture2D backButton;
+	
+	float buttony2 = Screen.height/10f;
+
+	void Update(){
+		buttony2 = Screen.height/10f;
+	}
+
+
+	void OnGUI () {
+		if (GUI.Button (new Rect (10,8.6f*buttony2,buttony2,buttony2), backButton)) {
+			//audio.clip = buttonPressClip;
+			//audio.Play();
+			Application.LoadLevel("Main_Menu");
+		}
+	}
+
+}
---
+++ Assets/Scripts/UI/GUIMainMenu.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUIMainMenu : MonoBehaviour {
+	
+	//for calculating button locations
+	float screenWidth = (Screen.width/2);
+	float screenHeight = (Screen.height/2);
+	float buttonx2 = Screen.width/5;
+	float buttony2 = Screen.height/8;
+	float buttonx1 = 1.0f;
+	
+	//button press sounds
+	//public AudioClip buttonPressClip;
+	
+	public Texture2D play;
+	public Texture2D highScores;
+	public Texture2D instructions;
+	public Texture2D credits;
+	public Texture2D quit;
+	
+	void Update(){
+		buttonx2 = Screen.width/5;
+		buttony2 = Screen.height/8;
+		screenWidth = (Screen.width/2);
+		screenHeight = (Screen.height/2);
+		buttonx1 = screenWidth / 1.2f;
+	}
+	//replace the string value with the texture2D
+	void OnGUI () {
+		if (GUI.Button (new Rect (buttonx1,screenHeight-1.25f*buttony2,buttonx2,buttony2), play)) {
+			//audio.clip = buttonPressClip;
+			//audio.Play();
+			Application.LoadLevel("Combined_Test_Scene");
+		}
+		if (GUI.Button (new Rect (buttonx1,screenHeight-0.25f*buttony2,buttonx2,buttony2), highScores)) {
+			//audio.clip = buttonPressClip;
+			//audio.Play();
+			Application.LoadLevel("High_Scores");
+		}
+		if (GUI.Button (new Rect (buttonx1,screenHeight+0.75f*buttony2,buttonx2,buttony2), instructions)) {
+			//audio.clip = buttonPressClip;
+			//audio.Play();
+			Application.LoadLevel("Instructions");
+		}
+		if (GUI.Button (new Rect (buttonx1,screenHeight+1.75f*buttony2,buttonx2,buttony2), credits)) {
+			//audio.clip = buttonPressClip;
+			//audio.Play();
+			Application.LoadLevel("Credits");
+		}
+		if (GUI.Button (new Rect (buttonx1,screenHeight+2.75f*buttony2,buttonx2,buttony2), quit)) {
+			//audio.clip = buttonPressClip;
+			//audio.Play();
+			Application.Quit();
+		}
+	}
+	
+}
