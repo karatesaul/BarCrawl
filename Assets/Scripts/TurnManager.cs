c58e0090578661fcd6b2ee6c808ea906e2c15553
Kyrsulli <kyrsulli@ucsc.edu>
2014-04-06 17:16:24 -0700

    Initial Enemy Spawner implementation
    Began implementation for the enemy spawner. I am unable to test it at
    current due to the bugs in the puzzle, which I will start to look into
    now. Additionally, there will be more things I need to do in Enemy.cs in
    order to get the arrays to be updated correctly when an enemy is
    defeated, so I will work on getting that put in once Steven is done with
    his work in that script.

--- Assets/Scripts/TurnManager.cs
+++ Assets/Scripts/TurnManager.cs
@@ -5,8 +5,12 @@ public class TurnManager : MonoBehaviour {
 
 	public PlayerCharacter player;
 	public int enemyCount;
+	public int maxEnemies;
+	public int spawnTimer;
 	public GameObject[] enemyObjs; 
-	public Enemy[] enemies; 
+	public Enemy[] enemies;
+	private GameObject enemyInstance;
+	public GameObject enemyReference;
 
 	//0 = none
 	//1 = player turn
@@ -15,13 +19,14 @@ public class TurnManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		maxEnemies = 4;
 		player = GameObject.Find("lamePC").GetComponent<PlayerCharacter>();
 		enemyObjs = GameObject.FindGameObjectsWithTag("enemy");
-		enemies = new Enemy[enemyObjs.Length];
+		enemies = new Enemy[maxEnemies];
 		for (int i = 0; i < enemyObjs.Length; i++) {
 			enemies[i] = enemyObjs[i].GetComponent<Enemy>();
 		}
-
+		spawnTimer = 0;
 		turn = 1;
 		enemyCount = 0;
 	}
@@ -33,10 +38,35 @@ public class TurnManager : MonoBehaviour {
 			//turn is set within PlayerCharacter.cs after exiting executeMode and attempting to move
 		} else if (turn == 2) {
 			//Debug.Log ("ENEMY TURN");
+			//player.score/100 = enemies defeated.
+			if((spawnTimer > 5 && enemyCount < (player.score/100) && enemyCount < maxEnemies) || enemyCount < 0){
+				shortenEnemyList(); //This should instead be called in Enemy, but I'll move it there later.
+				enemyInstance = Instantiate (enemyReference, new Vector2(2, 0), new Quaternion(0,0,0,0)) as GameObject;
+				enemyInstance.GetComponent<Enemy>().x = 4;
+				enemyInstance.GetComponent<Enemy>().y = 0;
+				for(int i = 0; i < enemies.Length; i++){
+					if(enemies[i] == null){
+						enemies[i] = enemyInstance.GetComponent<Enemy>();
+						i = enemies.Length;
+					}
+				}
+				spawnTimer = 0;
+			}
 			for (int i = 0; i < enemies.Length; i++) {
+				if(enemies[i]!=null)
 				enemies[i].isExecute = true;
 			}
 			turn = 1;
+			spawnTimer++;
+		}
+	}
+
+	void shortenEnemyList(){
+		for(int i = 1; i < maxEnemies; i++){
+			if(enemies[i-1] == null){
+				enemies[i-1] = enemies[i];
+				enemies[i] = null;
+			}
 		}
 	}
 }
