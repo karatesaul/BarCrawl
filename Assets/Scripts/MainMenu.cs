4f566eb5b542f8601a9e4b9574e8b8784fccc1ad
CodyIsAwesome <cchilder@ucsc.edu>
2014-04-06 21:31:03 -0700

    Main menu expansion
    Menu actually draws title because I forgot to commit that before.  There
    is a toggle button for the tutorial setting (actual tutorial yet to
    come), and there is a working transition into the high score screens and
    back.

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,12 +2,22 @@
 using System.Collections;
 
 public class MainMenu : MonoBehaviour {
-	bool displayMenu;
+	//whether or not to display this instead of puzzle
+	private bool displayMenu;
+	//images and tutorial settings
 	public Texture title;
+	public Texture yesTutorial;
+	public Texture noTutorial;
+	private bool tutorial;
 
 	// Use this for initialization
 	void Start () {
 		displayMenu = true;
+		//this will be changed to retreive the value from storage later.
+		//it assumes the first instance of the game to show the tutorial,
+		//and will be turned off after that unless the player selects the
+		//checkbox on this screen to turn it back on
+		tutorial = true;
 	}
 	
 	// Update is called once per frame
@@ -26,14 +36,26 @@ public class MainMenu : MonoBehaviour {
 		int x = Screen.width;
 		int y = Screen.height;
 		int buttonWidth  = Mathf.RoundToInt(x * 2.0f/3.0f);
-		int buttonHeight = Mathf.RoundToInt(y * 1.0f/4.0f);
+		int buttonHeight = Mathf.RoundToInt(y * 1.0f/6.0f);
+		//start game button
 		if(GUI.Button (new Rect(x/2 - buttonWidth/2, y/2, buttonWidth, buttonHeight), "Start game")){
 			//turn the menu off
 			displayMenu = false;
 			//turn the puzzle on
 			GameObject[] PM = GameObject.FindGameObjectsWithTag("PM");
 			//PM should only have 1 element
+			//PM[0].GetComponent<PuzzleManager>().loadTutorial();
 			PM[0].GetComponent <PuzzleManager>().puzzleActive = true;
+		}
+		//high score screen button
+		if(GUI.Button (new Rect(x/2 - buttonWidth/2, 2*y/3, buttonWidth, buttonHeight), "High Scores")){
+			//transition to high score screen
+			Application.LoadLevel("High_Scores");
+		}
+		//show tutorial setting
+		Texture t = (tutorial?yesTutorial:noTutorial);
+		if(GUI.Button (new Rect(Screen.width/2-t.width/2, Screen.height - t.height - 10, t.width, t.height), t)){
+			tutorial = !tutorial;
 		}
 	}
 }
