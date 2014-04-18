8612bac8168286a3ec601d69fb5e303ab8b2a67e
CodyIsAwesome <cchilder@ucsc.edu>
2014-04-17 17:35:10 -0700

    Randomization of levels
    It doesn't look like much because its randomly choosing from a set of 1,
    but all we need to do is add more levels to the list and it'll work.
    There are also options to force a level for debugging purposes in
    GUIMainMenu.cs

--- Assets/Scripts/UI/GUIMainMenu.cs
+++ Assets/Scripts/UI/GUIMainMenu.cs
@@ -2,56 +2,38 @@
 using System.Collections;
 
 public class GUIMainMenu : MonoBehaviour {
-	
-	//for calculating button locations
-	float screenWidth = (Screen.width/2);
-	float screenHeight = (Screen.height/2);
-	float buttonx2 = Screen.width/5;
-	float buttony2 = Screen.height/8;
-	float buttonx1 = 1.0f;
-	
-	//button press sounds
-	//public AudioClip buttonPressClip;
-	
-	public Texture2D play;
-	public Texture2D highScores;
-	public Texture2D instructions;
-	public Texture2D credits;
-	public Texture2D quit;
+
+	//add scene names to this
+	string[] possibleLevels = {"Combined_Test_Scene"};
+
+	static bool firstInit;//this will control the fancy MonoAxe Games presents stuff only on the first load
+	bool debuggingForceLoad;
+	string forceLevel;
+
+	void Start(){
+		firstInit = true;
+		/**************************************************************************************************
+		 *  Set this value to true and enter a level name to force test a level.  Change it back to false
+		 *  when done.  While false, it will choose randomly from the set.  It should be false for the 
+		 * final release
+		 * ***********************************************************************************************/
+		debuggingForceLoad = false;
+		forceLevel = "";
+	}
 	
 	void Update(){
-		buttonx2 = Screen.width/5;
-		buttony2 = Screen.height/8;
-		screenWidth = (Screen.width/2);
-		screenHeight = (Screen.height/2);
-		buttonx1 = screenWidth / 1.2f;
-	}
-	//replace the string value with the texture2D
-	void OnGUI () {
-		if (GUI.Button (new Rect (buttonx1,screenHeight-1.25f*buttony2,buttonx2,buttony2), play)) {
-			//audio.clip = buttonPressClip;
-			//audio.Play();
-			Application.LoadLevel("Combined_Test_Scene");
-		}
-		if (GUI.Button (new Rect (buttonx1,screenHeight-0.25f*buttony2,buttonx2,buttony2), highScores)) {
-			//audio.clip = buttonPressClip;
-			//audio.Play();
-			Application.LoadLevel("High_Scores");
-		}
-		if (GUI.Button (new Rect (buttonx1,screenHeight+0.75f*buttony2,buttonx2,buttony2), instructions)) {
-			//audio.clip = buttonPressClip;
-			//audio.Play();
-			Application.LoadLevel("Instructions");
-		}
-		if (GUI.Button (new Rect (buttonx1,screenHeight+1.75f*buttony2,buttonx2,buttony2), credits)) {
-			//audio.clip = buttonPressClip;
-			//audio.Play();
-			Application.LoadLevel("Credits");
-		}
-		if (GUI.Button (new Rect (buttonx1,screenHeight+2.75f*buttony2,buttonx2,buttony2), quit)) {
-			//audio.clip = buttonPressClip;
-			//audio.Play();
-			Application.Quit();
+		if(firstInit){
+			//show the team logo and shit
+			//turn firstInit to false when done
+			firstInit = false;
+		}else{
+			if(debuggingForceLoad){
+				Application.LoadLevel(forceLevel);
+			}else{
+				//get a random number
+				int rand = (int)Mathf.Floor(Random.Range(0, possibleLevels.Length));
+				Application.LoadLevel(possibleLevels[rand]);
+			}
 		}
 	}
 	
