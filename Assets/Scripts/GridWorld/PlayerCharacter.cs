2ab6f9dfd1c15ef8fb05a12525e8e23073e175a4
littlebeast <littlebeast@sbcglobal.net>
2014-04-30 15:38:05 -0700

    Converted PlayerCharacter to use a Queue internally for moves
    Interface with PuzzleManager is the same, though I'm considering changing that as well; the current code is ugly.
    Incidentally, is there a reason that we have Move and TokenType as two separate but identical enums?

--- Assets/Scripts/GridWorld/PlayerCharacter.cs
+++ Assets/Scripts/GridWorld/PlayerCharacter.cs
@@ -4,39 +4,42 @@ using System.Collections.Generic;
 
 public class PlayerCharacter : FightingEntity {
 
-	public Move[] movelist;
-	public TokenType[] moveInput;
+	public Queue<Move> moveQueue;
+
+	//oh my god, that is ugly.
+	public TokenType[] moveInput; 
 	public bool fillUp;
+
 	public TurnManager tm;
 	public MainMenu menu;
-	private int currCombo;
-	public bool executeMode;
-	public int score;
-	private bool exit;
-	private int timer;
-	public int startingHealth = 100;
 	public Vector3 cameraOffset;
 	public Camera worldCamera;
+
+	public bool executeMode;
+	private int timer;
+	
+	public int score;
+
+	public int maxHealth = 100;
+	public int startingHealth = 100;
+
 	public int blunderDamage = 5;
 	public int healAmount = 10;
-	public int maxHealth = 100;
 	
 	// Use this for initialization
 	protected override void Start () {
 		tm = GameObject.Find("Player").GetComponent<TurnManager>();
 		menu = GameObject.Find("MainMenu").GetComponent<MainMenu>();
 
-		exit = false;
+
 		fillUp = false;
 		score = 0;
 		health = startingHealth;
 		timer = 30;
 		executeMode = false;
-		movelist = new Move[20];
-		currCombo = 0;
-		for(int i = 0; i < 20; i++){
-			movelist[i] = Move.None;
-		}
+	
+
+		moveQueue = new Queue<Move> (10);
 
 		foeTag = "enemy";
 
@@ -52,141 +55,92 @@ public class PlayerCharacter : FightingEntity {
 	protected override void Update () 
 	{
 		base.Update ();
+
+		//camera follows player!
 		worldCamera.gameObject.transform.position = gameObject.transform.position + cameraOffset;
 
 		if (tm.turn == 1) 
 		{
 			if (!executeMode) 
 			{
-				if(fillUp){
-			for(int i = 0; i < 20; i++){
-				if(moveInput.Length == i){
-					Debug.Log ("ran out of moves at " + i);
-					break;
-				}
-				switch(moveInput[i]){
-				case TokenType.Left:
-					Debug.Log ("Left move queued.");
-					movelist[i] = Move.Left;
-					//score = score + 25;
-					break;
-				case TokenType.Right:
-					Debug.Log ("Right move queued.");
-					movelist[i] = Move.Right;
-					//score = score + 25;
-					break;
-				case TokenType.Up:
-					Debug.Log ("Up move queued.");
-					movelist[i] = Move.Up;
-					//score = score + 25;
-					break;
-				case TokenType.Down:
-					Debug.Log ("Down move queued.");
-					movelist[i] = Move.Down;
-					//score = score + 25;
-					break;
-				case TokenType.Attack:
-					Debug.Log ("Fight move queued.");
-					movelist[i] = Move.Fight;
-					score = score + 25;
-					break;
-				case TokenType.Heal:
-					Debug.Log ("Heal move queued.");
-					movelist[i] = Move.Heal;
-					break;
-				default:
-					Debug.Log ("No move queued.");
-					movelist[i] = Move.None;
-					break;
 
+				//this
+				//is so friggin ugly
+				if(fillUp)
+				{
+					for(int i = 0; i < moveInput.Length; i++)
+					{
+						switch(moveInput[i]){
+						case TokenType.Left:
+							//Debug.Log ("Left move queued.");
+							moveQueue.Enqueue(Move.Left);
+							//score = score + 25;
+							break;
+						case TokenType.Right:
+							//Debug.Log ("Right move queued.");
+							moveQueue.Enqueue(Move.Right);
+							//score = score + 25;
+							break;
+						case TokenType.Up:
+							//Debug.Log ("Up move queued.");
+							moveQueue.Enqueue(Move.Up);
+							//score = score + 25;
+							break;
+						case TokenType.Down:
+							//Debug.Log ("Down move queued.");
+							moveQueue.Enqueue(Move.Down);
+							//score = score + 25;
+							break;
+						case TokenType.Attack:
+							//Debug.Log ("Fight move queued.");
+							moveQueue.Enqueue(Move.Fight);
+							//score = score + 25;
+							break;
+						case TokenType.Heal:
+							//Debug.Log ("Heal move queued.");
+							moveQueue.Enqueue(Move.Heal);
+							break;
+						default:
+							Debug.Log ("Move.None queued.  Why did this happen?");
+							moveQueue.Enqueue(Move.None);
+							break;
+
+						}
+					}
+					fillUp = false;
+					executeMode = true;
 				}
-			}
-			fillUp = false;
-			executeMode = true;
-						}
-		/*
-						if (Input.GetKeyDown (KeyCode.LeftArrow)) {
-								if (currCombo < 10) {
-										movelist [currCombo] = Move.Left;
-										currCombo++;
-										Debug.Log ("Left move queued.");
-								} else
-										Debug.Log ("Queue full!");
-								//AttemptMove (Move.Left);
-						}
-						if (Input.GetKeyDown (KeyCode.RightArrow)) {
-								if (currCombo < 10) {
-										movelist [currCombo] = Move.Right;
-										currCombo++;
-										Debug.Log ("Right move queued.");
-								} else
-										Debug.Log ("Queue full!");
-								//AttemptMove (Move.Right);
-						}
-						if (Input.GetKeyDown (KeyCode.UpArrow)) {
-								if (currCombo < 10) {
-										movelist [currCombo] = Move.Up;
-										currCombo++;
-										Debug.Log ("Up move queued.");
-								} else
-										Debug.Log ("Queue full!");
-								//AttemptMove (Move.Up);
-						}
-						if (Input.GetKeyDown (KeyCode.DownArrow)) {
-								if (currCombo < 10) {
-										movelist [currCombo] = Move.Down;
-										currCombo++;
-										Debug.Log ("Down move queued.");
-								} else
-										Debug.Log ("Queue full!");
-								//AttemptMove (Move.Down);
-						}
-						if (Input.GetKeyDown (KeyCode.F)) {
-								if(currCombo < 10){
-									movelist [currCombo] = Move.Fight;
-									currCombo++;
-									Debug.Log ("Fight move queued.");
-								}
-								else
-									Debug.Log ("Queue full!");
-								//AttemptMove (Move.Down);
-						}
-						if (Input.GetKeyDown (KeyCode.Space)) {
-								currCombo = 0;
-								executeMode = true;
-								tm.turn = 1;
-								Debug.Log ("Execute mode entered.");
-						}
-				*/
 			} 
 			else 
 			{
-				if (currCombo > 19 || currCombo < 0) {
-					currCombo = 0;
-					exit = true;
-				}
-				if (exit || movelist [currCombo] == Move.None) {
-					for (int i = 0; i < 20; i++) {
-							movelist [i] = Move.None;
-					}
-					//Debug.Log ("Execute mode exited.");
-					exit = false;
-					currCombo = 0;
-					timer = 30;
+				//this section: executeMode == true;
+
+				if (moveQueue.Count == 0) {
+
+					Debug.Log ("Player queue emptied.");
+
+					//end player turn
 					executeMode = false;
+
+					//prevent delay when player's next turn starts
+					timer = 30;
+
+					//signal enemies to take turn.
 					tm.turn = 2;
 					return;
 				}
+
+				//will probably want to change this to be controlled by the animations
+				//you know, once we have walking animations in
 				timer++;
 				if (timer > 30) {
-					AttemptMove (movelist [currCombo]);
-					currCombo++;
+
+					AttemptMove(moveQueue.Dequeue());
+
 					timer = 0;
 				}
 			}
 		}
-
-			//make sure the sprite is the right color
 
 	}
 
