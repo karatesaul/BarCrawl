7a6dd8f0f678bee557c48751eca8b4b166c161a8
littlebeast <littlebeast@sbcglobal.net>
2014-05-08 17:11:33 -0700

    Bottle throwing is a thing!
    The timing isn't great - the bottle appears before the throwing animation - but it's there.

---
+++ Assets/Scripts/Bottle.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bottle : MonoBehaviour {
+
+	public int target_x;
+	public int target_y;
+
+	public Vector3 targetOffset = new Vector3 (.25f, .1f, 0);
+
+	public bool spinClockwise;
+
+	public float throwSpeed = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(spinClockwise)
+			gameObject.transform.Rotate (0, 0, -10);
+		else
+			gameObject.transform.Rotate (0, 0, 10);
+
+		Vector3 targetPos = (Vector3)GridManager.getTransformPosition (target_x, target_y) + targetOffset;
+		Vector3 ownPos = gameObject.transform.position;
+
+		ownPos.x = approach (targetPos.x, ownPos.x, throwSpeed);
+		ownPos.y = approach (targetPos.y, ownPos.y, throwSpeed);
+
+		if (ownPos.x == targetPos.x && ownPos.y == targetPos.y)
+		{
+			Destroy(this.gameObject);
+		}
+
+		gameObject.transform.position = ownPos;
+	}
+
+	private float approach(float target, float pos, float speed)
+	{
+		if (pos < target)
+		{
+			pos += speed;
+			if(pos > target)
+				pos = target;
+		}
+
+		if (pos > target)
+		{
+			pos -= speed;
+			if(pos < target)
+				pos = target;
+		}
+
+		return pos;
+	}
+}
--- Assets/Scripts/GridWorld/RangedEnemy.cs
+++ Assets/Scripts/GridWorld/RangedEnemy.cs
@@ -5,6 +5,9 @@ public class RangedEnemy : Enemy {
 
 	bool hasBottle;
 
+	public Bottle prefab;
+	private Vector3 bottleOffset = new Vector3(.25f, .1f, 0);
+
 	// Use this for initialization
 	protected override void Start () {
 
@@ -35,6 +38,12 @@ public class RangedEnemy : Enemy {
 		bool success = base.AttemptMove(move);
 		if (move == Move.Fight && success)
 		{
+			Bottle proj = (Bottle)Instantiate(prefab, gameObject.transform.position + bottleOffset, Quaternion.identity);
+
+			//this is kind of unwise, but it works
+			proj.target_x = player.x;
+			proj.target_y = player.y;
+
 			hasBottle = false;
 		}
 
