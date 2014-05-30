95a6b3b3140fd9483acb9bf328bce67fc04fee55
CodyIsAwesome <cchilder@ucsc.edu>
2014-05-29 19:06:20 -0700

    More edits to high scores
    Still needs slight formatting, but it is much better the way it is now.
    Facebook button also takes you to the game's facebook page.

--- Assets/Scripts/UI/GUIBackMainMenuButton.cs
+++ Assets/Scripts/UI/GUIBackMainMenuButton.cs
@@ -10,39 +10,35 @@ public class GUIBackMainMenuButton : MonoBehaviour {
 	public Texture findUsOnFacebook;
 	public Font chewy;
 
-	private GUIStyle text;
-	private GUIStyle image;
+	private GUIStyle textStyle;
+	private GUIStyle imageStyle;
 
 	//float buttony2 = Screen.height/9f;
 
 	void Update(){
 		//buttony2 = Screen.height/9f;
-		text = new GUIStyle();
-		text.font = chewy;
-		text.fontSize = Screen.width / 10;
-		text.alignment = TextAnchor.MiddleCenter;
-		text.richText = true;
-		image = new GUIStyle();
+		textStyle = new GUIStyle();
+		textStyle.font = chewy;
+		textStyle.fontSize = Screen.width / 10;
+		textStyle.alignment = TextAnchor.MiddleCenter;
+		textStyle.richText = true;
+		imageStyle = new GUIStyle();
 	}
 
 
 	void OnGUI () {
 		//if (GUI.Button (new Rect (Screen.width/1.4f,Screen.height/1.2f,buttony2,buttony2), backButton)) {
 
-		if(GUI.Button (new Rect(0, Screen.height-100, Screen.width, 100), "<color=#ffffff>Continue my bar crawl</color>", text)){
-			//audio.clip = buttonPressClip;
-			//audio.Play();
+		if(GUI.Button (new Rect(0, Screen.height-100, Screen.width, 100), "<color=#ffffff>On to the next bar!</color>", textStyle)){
 			Application.LoadLevel("Main_Menu");
 		}
-
-		//if (GUI.Button (new Rect (Screen.width/8.9f,Screen.height/1.2f,buttony2,buttony2), fbPlaceholder)) {
-		if(GUI.Button (new Rect(Screen.height - 100 - findUsOnFacebook.height, Screen.width/2 - findUsOnFacebook.width/2, 
+		textStyle.fontSize = Mathf.RoundToInt (textStyle.fontSize * 0.8f);//shrink the font a smidge so it fits on screen
+		GUI.Label (new Rect(0, Screen.height - 100 - findUsOnFacebook.height - 50, Screen.width, 50), 
+		           "<color=#ffffff><i>Come tell us how you did!</i></color>", textStyle);
+		textStyle.fontSize = Screen.width / 10;
+		if(GUI.Button (new Rect(Screen.width/2 - findUsOnFacebook.width/2, Screen.height - 100 - findUsOnFacebook.height,
 		                        findUsOnFacebook.width, findUsOnFacebook.height),
-		               findUsOnFacebook/*, image*/)){
-		//Debug.Log ("w: " + findUsOnFacebook.width + " h: " + findUsOnFacebook.height);
-		//if(GUI.Button (new Rect(0, 0, findUsOnFacebook.width, findUsOnFacebook.height), "FB")){
-			//audio.clip = buttonPressClip;
-			//audio.Play();
+		               findUsOnFacebook, imageStyle)){
 			Application.OpenURL("https://www.facebook.com/BarCrawlANightToForget");
 		}
 	}
