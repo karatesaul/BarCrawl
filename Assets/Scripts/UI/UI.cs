5f0122de9a2dbd07dc8addeebebcbe8d7f02a263
kvelury <kvelury@ucsc.edu>
2014-06-03 22:28:27 -0700

    Added new UI elements, visual timer, reset puzzle
    new sprites, etc. for healthbar, visual timer, reset puzzle

--- Assets/Scripts/UI/UI.cs
+++ Assets/Scripts/UI/UI.cs
@@ -2,17 +2,24 @@
 using System.Collections;
 
 public class UI : MonoBehaviour {
-
+	
 	public bool showUI;
 	public TurnManager tm;
 	public PlayerCharacter pc;
 	public PuzzleManager pm;
 	public Texture2D healthbar;
+	public Texture2D healthbg;
 	public Texture2D emptybar;
 	public Texture2D clock1;
 	public Texture2D clock2;
 	public Texture2D clock3;
 	public Texture2D clock4;
+	public Texture2D clock5;
+	public Texture2D clock6;
+	public Texture2D beer0;
+	public Texture2D beer1;
+	public Texture2D beer2;
+	public Texture2D beer3;
 	public Font chewy;
 	
 	public bool combo2;
@@ -24,10 +31,12 @@ public class UI : MonoBehaviour {
 	public bool tut3;
 	public bool showNumericHealth;
 	private float timer;
-
+	
 	private GUIStyle healthtext;
 	private GUIStyle scoreText;
 	private GUIStyle comboText;
+	private GUIStyle buttonStyle;
+	
 	
 	// Use this for initialization
 	void Start () {
@@ -54,6 +63,8 @@ public class UI : MonoBehaviour {
 		comboText.fontSize = Screen.width/25;
 		comboText.normal.textColor = Color.white;
 		comboText.alignment = TextAnchor.UpperCenter;
+		
+		buttonStyle = new GUIStyle();
 		
 		timer = 3.0f;
 		combo2 = false;
@@ -103,14 +114,19 @@ public class UI : MonoBehaviour {
 		if (showUI) {
 			//healthbar
 			GUI.DrawTexture (new Rect (Screen.width/2 - Screen.width/4, Screen.height/25,
-			                           Screen.width/2, Screen.height/30), emptybar);
-			GUI.DrawTexture (new Rect (Screen.width/2 - Screen.width/4, Screen.height/25,
+			                           Screen.width/2, Screen.height/30), healthbg);
+			
+			GUI.DrawTexture (new Rect (Screen.width/2 - (Screen.width/2)*((float)pc.health/pc.startingHealth)/2, Screen.height/25,
 			                           (Screen.width/2)* ((float)pc.health/pc.startingHealth), Screen.height/30), healthbar);
+			
+			GUI.DrawTexture (new Rect (Screen.width/2 - Screen.width/4, Screen.height/25,
+			                           Screen.width/2, Screen.height/30), emptybar);
+			
 			if (showNumericHealth) DrawOutline (new Rect (Screen.width / 2, Screen.height/25, 0, 0), pc.health + "/100", healthtext, Color.black);
 			
 			//score
 			DrawOutline (new Rect (Screen.width / 2, Screen.height/20 + 5, 0, 0), "Score: " + Scores.total.ToString(), scoreText, Color.black);
-
+			
 			//combotext
 			if (combo2) DrawOutline(new Rect (Screen.width/2, Screen.height/15 + 20, 0, 0), "2X combo!", comboText, Color.black);
 			if (combo3) DrawOutline(new Rect (Screen.width/2, Screen.height/15 + 20, 0, 0), "3X combo!", comboText, Color.black);
@@ -118,15 +134,29 @@ public class UI : MonoBehaviour {
 			if (combo5) DrawOutline(new Rect (Screen.width/2, Screen.height/15 + 20, 0, 0), "Crazy Combo!", comboText, Color.red);
 			
 			//visual timer
-			if (pm.currTime <= 400 && pm.currTime > 300) GUI.DrawTexture(new Rect (Screen.width/25, Screen.height/8, 4*Screen.width/25, 4*Screen.height/45), clock4);
-			else if (pm.currTime <= 300 && pm.currTime > 200) GUI.DrawTexture(new Rect (Screen.width/25, Screen.height/8, 4*Screen.width/25, 4*Screen.height/45), clock3);
-			else if (pm.currTime <= 200 && pm.currTime > 100) GUI.DrawTexture(new Rect (Screen.width/25, Screen.height/8, 4*Screen.width/25, 4*Screen.height/45), clock2);
-			else if (pm.currTime <= 100) GUI.DrawTexture(new Rect (Screen.width/25, Screen.height/8, 4*Screen.width/25, 4*Screen.height/45), clock1);
+			if (pm.currTime <= 600 && pm.currTime > 500) GUI.DrawTexture(new Rect (Screen.width/2 - 2*Screen.width/25, Screen.width * 5/6 - Screen.width/6, 4*Screen.width/25, 4*Screen.height/45), clock1);
+			else if (pm.currTime <= 500 && pm.currTime > 400) GUI.DrawTexture(new Rect (Screen.width/2 - 2*Screen.width/25, Screen.width * 5/6 - Screen.width/6, 4*Screen.width/25, 4*Screen.height/45), clock2);
+			else if (pm.currTime <= 400 && pm.currTime > 300) GUI.DrawTexture(new Rect (Screen.width/2 - 2*Screen.width/25, Screen.width * 5/6 - Screen.width/6, 4*Screen.width/25, 4*Screen.height/45), clock3);
+			else if (pm.currTime <= 300 && pm.currTime > 200) GUI.DrawTexture(new Rect (Screen.width/2 - 2*Screen.width/25, Screen.width * 5/6 - Screen.width/6, 4*Screen.width/25, 4*Screen.height/45), clock4);
+			else if (pm.currTime <= 200 && pm.currTime > 100) GUI.DrawTexture(new Rect (Screen.width/2 - 2*Screen.width/25, Screen.width * 5/6 - Screen.width/6, 4*Screen.width/25, 4*Screen.height/45), clock5);
+			else if (pm.currTime <= 100) GUI.DrawTexture(new Rect (Screen.width/2 - 2*Screen.width/25, Screen.width * 5/6 - Screen.width/6, 4*Screen.width/25, 4*Screen.height/45), clock6);
 			else {
 				//draw nothing
 			}
-
+			
 			//reset button
+			if (tm.coolDownTimer > 4) {
+				if (GUI.Button (new Rect(Screen.width/25, Screen.height/45, 4*Screen.width/25, 4*Screen.height/45), beer3, buttonStyle) && tm.turn ==1) {
+					pm.ResetPuzzle();
+					tm.coolDownTimer = 0;
+				}
+			}
+			else if (tm.coolDownTimer == 4) GUI.Box (new Rect(Screen.width/25, Screen.height/45, 4*Screen.width/25, 4*Screen.height/45), beer2, buttonStyle);
+			else if (tm.coolDownTimer == 2 || tm.coolDownTimer == 3) 
+				GUI.Box (new Rect(Screen.width/25, Screen.height/45, 4*Screen.width/25, 4*Screen.height/45), beer1, buttonStyle);
+			else if (tm.coolDownTimer <= 1) GUI.Box (new Rect(Screen.width/25, Screen.height/45, 4*Screen.width/25, 4*Screen.height/45), beer0, buttonStyle);
+			
+			/*
 			if (tm.coolDownTimer > 4) {
 				if(GUI.Button (new Rect(Screen.width/25, Screen.height/45, 4*Screen.width/25, 4*Screen.height/45), "Reset the\nPuzzle!") && tm.turn == 1){
 					pm.ResetPuzzle();
@@ -136,6 +166,7 @@ public class UI : MonoBehaviour {
 			else{
 				GUI.Box (new Rect(Screen.width/25, Screen.height/45, 4*Screen.width/25, 4*Screen.height/45), "Reset\nRecharging!");
 			}
+			*/
 			
 			//tutorial text
 			if (tut1) DrawOutline (new Rect (Screen.width / 2, Screen.height / 3, 0, 0), "Combine 3 punches to fight enemies!", comboText, Color.black);
